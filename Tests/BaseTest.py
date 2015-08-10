803bb20a493e36acf99e404e7d2068e14b9d401e
lcaravajalDs <lcaravajal@devspark.com>
2015-08-10 18:11:44 -0300

    Update BaseTest.py

--- Tests/BaseTest.py
+++ Tests/BaseTest.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 
 
 class BaseTest(unittest.TestCase):
-    configfile=parser()
     def setUp(self):
         self.driver=webdriver.Firefox()
         self.driver.implicitly_wait(10)
