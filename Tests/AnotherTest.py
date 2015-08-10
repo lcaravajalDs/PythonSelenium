fdb9cc364c4376d9408e05fffb97dd78d25ae2d6
lcaravajalDs <lcaravajal@devspark.com>
2015-08-10 18:06:19 -0300

    Update AnotherTest.py

--- Tests/AnotherTest.py
+++ Tests/AnotherTest.py
@@ -4,12 +4,10 @@ Created on 7/8/2015
 @author: Luciano
 '''
 import unittest
+from Tests.BaseTest import BaseTest
 
-
-class AnotherTest(unittest.TestCase):
+class AnotherTest(BaseTest):
 
     def test_name(self):
         assert 2 in [1,3]
 
-if __name__ == '__main__':
-    unittest.main()
