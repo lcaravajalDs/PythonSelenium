8e3706e200af2e9092c08f52b07966d8b7daa85a
lcaravajalDs <lcaravajal@devspark.com>
2015-08-10 17:50:55 -0300

    Update BaseTest.py

--- Tests/BaseTest.py
+++ Tests/BaseTest.py
@@ -3,12 +3,19 @@ import unittest
 from Config.ConfigParser import parser
 from selenium import webdriver
 
-class BaseTest(unittest.TestCase):
-        def setUp(self):
-                self.driver=webdriver.Chrome()
-                self.driver.implicitly_wait(10)
-                self.driver.get("http://www.python.org")
-        
 
-        def tearDown(self):
-                self.driver.close()
+class BaseTest(unittest.TestCase):
+    configfile=parser()
+    def setUp(self):
+        print ('algo')
+        self.driver=webdriver.Firefox()
+        self.driver.implicitly_wait(10)
+        self.driver.get("http://www.python.org")
+    def test_lala(self):
+        print ('lala')
+
+    def tearDown(self):
+        self.driver.close()
+        
+if __name__ == '__main__':
+    unittest.main()
