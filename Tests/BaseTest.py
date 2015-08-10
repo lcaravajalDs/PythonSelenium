c400c77d9ccbe539d8e5ef77f0229ce108752232
lcaravajalDs <lcaravajal@devspark.com>
2015-08-10 14:33:52 -0300

    Update BaseTest.py

--- Tests/BaseTest.py
+++ Tests/BaseTest.py
@@ -4,8 +4,7 @@ from Config.ConfigParser import parser
 from selenium import webdriver
 
 class BaseTest(unittest.TestCase):
-    configfile=parser()
-    def setUp(self):
+        def setUp(self):
         print ('algo')
         self.driver=webdriver.Chrome()
         self.driver.implicitly_wait(10)
