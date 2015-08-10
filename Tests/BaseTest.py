b1ce455bda048b3121d0222709af686811a92b9d
lcaravajalDs <lcaravajal@devspark.com>
2015-08-10 14:23:29 -0300

    Update BaseTest.py

--- Tests/BaseTest.py
+++ Tests/BaseTest.py
@@ -6,10 +6,8 @@ from Config.ConfigParser import parser
 class BaseTest(unittest.TestCase):
     configfile=parser()
     def setUp(self):
-        self.driver = self.configfile.getBrowser('1')
-        self.driver.implicitly_wait(10)
-        env=self.configfile.get_url()
-        self.driver.get(env)
+       self.driver=webdriver.Firefox()
+        self.driver.get("http://www.python.org")
     def test_lala(self):
         print ('lala')
 
