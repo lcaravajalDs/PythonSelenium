298fdefbcf7a3c985697685e441b37b8c431b4d7
lcaravajalDs <lcaravajal@devspark.com>
2015-08-10 14:38:21 -0300

    Update BaseTest.py

--- Tests/BaseTest.py
+++ Tests/BaseTest.py
@@ -10,8 +10,5 @@ class BaseTest(unittest.TestCase):
                 self.driver.get("http://www.python.org")
         
 
-    def tearDown(self):
-        self.driver.close()
-        
-if __name__ == '__main__':
-    unittest.main()
+        def tearDown(self):
+                self.driver.close()
