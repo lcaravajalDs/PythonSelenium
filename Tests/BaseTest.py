7cb53f74d9c89166bdd0130465b005b10f8e9764
lcaravajalDs <lcaravajal@devspark.com>
2015-08-10 18:11:04 -0300

    Update BaseTest.py

--- Tests/BaseTest.py
+++ Tests/BaseTest.py
@@ -1,26 +1,16 @@
 import unittest
 
-from Config.ConfigParser import parser
+#from Config.ConfigParser import parser
 from selenium import webdriver
-from Page.SearchResultsPage import SearchResultsPage
-from Page.MainPage import MainPage
 
 
 
 class BaseTest(unittest.TestCase):
     configfile=parser()
     def setUp(self):
-        print ('algo')
         self.driver=webdriver.Firefox()
         self.driver.implicitly_wait(10)
         self.driver.get("http://www.python.org")
-        main_page = MainPage(self.driver)
-        main_page.search_field="pycon"
-        main_page.click_go_button()
-        search_results_page = SearchResultsPage(self.driver)
-        assert search_results_page.is_results_found(), "No results found."
-    def test_lala(self):
-        print ('lala')
 
     def tearDown(self):
         self.driver.close()
