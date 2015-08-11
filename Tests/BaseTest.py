679b33f65b91ecb491966795f5d3a8e7c21c27a4
lcaravajalDs <lcaravajal@devspark.com>
2015-08-11 10:57:05 -0300

    Upadte Base test

--- Tests/BaseTest.py
+++ Tests/BaseTest.py
@@ -1,16 +1,15 @@
 import unittest
 
 #from Config.ConfigParser import parser
-from selenium import webdriver
 from Config.ConfigParser import parser
 
 class BaseTest(unittest.TestCase):
     configfile=parser()
     def setUp(self):
         self.driver = self.configfile.getBrowser('1')
-+       self.driver.implicitly_wait(10)
-+       env=self.configfile.get_url()
-+       self.driver.get(env)
+        self.driver.implicitly_wait(10)
+        env=self.configfile.get_url()
+        self.driver.get(env)
 
     def tearDown(self):
         self.driver.close()
