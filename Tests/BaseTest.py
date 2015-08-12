603261e157608b679fc3bf6845a75b141c6cd454
lcaravajalDs <lcaravajal@devspark.com>
2015-08-12 09:54:47 -0300

    testing

--- Tests/BaseTest.py
+++ Tests/BaseTest.py
@@ -2,25 +2,14 @@ import unittest
 
 #from Config.ConfigParser import parser
 from Config.ConfigParser import parser
-import datetime
 
 class BaseTest(unittest.TestCase):
     configfile=parser()
     def setUp(self):
-        current_time = datetime.datetime.now().time()
-        print (current_time.isoformat())
         self.driver = self.configfile.getBrowser('1')
-        current_time = datetime.datetime.now().time()
-        print (current_time.isoformat())
         self.driver.implicitly_wait(10)
-        current_time = datetime.datetime.now().time()
-        print (current_time.isoformat())
         env=self.configfile.get_url()
-        current_time = datetime.datetime.now().time()
-        print (current_time.isoformat())
         self.driver.get(env)
-        current_time = datetime.datetime.now().time()
-        print (current_time.isoformat())        
 
     def tearDown(self):
         self.driver.close()
